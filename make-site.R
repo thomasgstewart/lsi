fca2e4a6129f25361435a4f8a23f37181b42d512
thomasgstewart <thomas.stewart@vanderbilt.edu>
2018-05-10 19:46:10 -0500

    "$@"

--- make-site.R
+++ make-site.R
@@ -2,11 +2,11 @@ require(tgsify)
 
 make_index <- function(){
   
-  posts <- list.files(path = "./posts/", pattern = ".\\.Rmd")
+  posts <- list.files(pattern = "^[0-9]{4}-[0-9]{2}-[0-9]{2}.*\\.Rmd")
   locs <- gsub("\\.Rmd$",".html", posts)
   titles <- dates <- rep(NA, length(posts))
   for(i in 1:length(posts)){
-    titles[i] <- readLines(con = "./posts/" %|% posts[i], n = 2) %>% 
+    titles[i] <- readLines(con = "./" %|% posts[i], n = 2) %>% 
       ve(2) %>% 
       gsub("title: ", "", .)
     dates[i] <- posts[i] %>% substr(1,10)
@@ -14,7 +14,7 @@ make_index <- function(){
   
   
   head <- "---\ntitle: Posts\n---\n\n"
-  body <- "- [" %|% titles %|% "](./posts/_site/" %|% locs %|% ")\n"
+  body <- "- [" %|% titles %|% "](" %|% locs %|% ")\n"
   cat(head, body, sep = "", file = "index.Rmd")
 }
 
